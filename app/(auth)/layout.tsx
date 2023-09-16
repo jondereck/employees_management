c1a983f5e91f3c902133a7ba9f99af31cc5e02fa
jondereck <jonderecknifas@gmail.com>
2023-09-16 22:45:05 +0800

    update auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
-export  const AuthLayout = ({
+export  default function AuthLayout ({
   children
 }: {
   children: React.ReactNode;
-}) => {
+}) {
   return (  
     <div className="flex items-center justify-center h-full">
       {children}
     </div>
   );
 }
+
+
