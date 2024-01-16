9f9a4b1f0775f08e001b03c2ea6f8f7d15718aab
jondereck <jonderecknifas@gmail.com>
2024-01-16 15:55:25 +0800

    update-loader

--- components/loading-state.tsx
+++ components/loading-state.tsx
@@ -1,16 +1,26 @@
 "use client";
 
+import Image from "next/image";
 import { PuffLoader } from "react-spinners";
 
 const LoadingState = () => {
-  return (  
-    <div className="h-full flex flex-col  justify-center items-center">
-      <PuffLoader 
+  return (
+    <div className="h-full flex flex-col items-center
+    justify-center gap-y-4  ">
+      {/* <PuffLoader 
         size={100}
         color="red"
-      />
+      /> */}
+
+      <div className="w-16 h-16  animate-spin">
+        <Image
+          fill
+          alt="logo"
+          src={'https://res.cloudinary.com/ddzjzrqrj/image/upload/v1702523620/Lingayen-3-removebg-preview_lmhivo.png'}
+        />
+      </div>
     </div>
   );
 }
- 
+
 export default LoadingState;
