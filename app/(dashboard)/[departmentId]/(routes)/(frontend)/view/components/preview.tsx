8f95da19c93026f5e7fbe3001dc987dabf1d0ff0
jondereck <jonderecknifas@gmail.com>
2024-04-17 14:31:19 +0800

    fix bug

--- app/(dashboard)/[departmentId]/(routes)/(frontend)/view/components/preview.tsx
+++ app/(dashboard)/[departmentId]/(routes)/(frontend)/view/components/preview.tsx
@@ -19,11 +19,11 @@ const PreviewModal = () => {
       open={previewModal.isOpen}
       onClose={previewModal.onClose}
     >
-      <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-        <div className="sm:col-span-4 lg:col-span-5">
+     <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-8">
+        <div className="col-span-1">
           <Gallery images={employee.images}/>
         </div>
-        <div className="sm:colspan-8 lg:col-span-7">
+        <div className="sm:colspan-1 md:col-span-2 lg:col-span-2">
           <Info data={employee}/>
         </div>
       </div>
--- app/(dashboard)/[departmentId]/(routes)/(frontend)/view/components/ui/employee-list.tsx
+++ app/(dashboard)/[departmentId]/(routes)/(frontend)/view/components/ui/employee-list.tsx
@@ -43,7 +43,7 @@ const EmployeeList = ({
     <div className="flex-row justify-center jusspace-y-4 ">
     <h3 className="font-bold text-3xl">{title}</h3>
     {displayedEmployee.length === 0 && <NoResults />}
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-x-2">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-x-2 space-y-2">
       {displayedEmployee.map((item) => (
         <EmployeeCard key={item.id} data={item} />
       ))}
