1f2261539f48fb7dcfeee5ab73e46ba67aa7b7e5
jondereck <jonderecknifas@gmail.com>
2024-07-13 19:15:55 +0800

    fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import LoadingState from "@/components/loading-state";
 
 const Loading = () => {
   return ( 
-    <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50 p-4">
-      <div className="p-4  rounded-lg shadow-lg sm:fixed sm:right-0 sm:bottom-0">
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50 p-4">
+      <div className="p-4  rounded-lg  sm:fixed sm:right-0 sm:bottom-0">
         <LoadingState />
       </div>
     </div>
