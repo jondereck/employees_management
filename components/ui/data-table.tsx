a3edc2cdea12db1aeb7e0bea267c1ef9456c4107
jondereck <jonderecknifas@gmail.com>
2025-08-28 14:05:29 +0800

    fix: add 100 as an option for page size in DataTable component

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -151,7 +151,7 @@ export function DataTable<TData, TValue>({
                 <SelectValue placeholder={table.getState().pagination.pageSize} />
               </SelectTrigger>
               <SelectContent side="top">
-                {[10, 20, 30, 40, 50].map((pageSize) => (
+                {[10, 20, 30, 40, 50, 100].map((pageSize) => (
                   <SelectItem key={pageSize} value={`${pageSize}`}>
                     {pageSize}
                   </SelectItem>
